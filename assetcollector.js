53dc796e962795f0441b429349d19c80a7aebd19
Daniel Nelson <dnelson@centresource.com>
2012-02-06 12:58:22 -0600

    since postMessage works on IE8, and we only need to support as far back as IE8 for asset collection, switch to the faster (and lower impact on our server) approach of messaging the iframe directly with the image sources and dimensions

--- assetcollector.js
+++ assetcollector.js
@@ -3,15 +3,14 @@ populrme = window.populrme || { env: 't' };
 populrme.appHosts = { d: 'lvh.me:3000', p: 'populr.me', s: 'staging.populr.me', t: 'populr.me' };
 populrme.appHost = populrme.appHosts[populrme.env];
 populrme.protocol = populrme.env == 'd' ? 'http:' : window.location.protocol;
-populrme.id = String(new Date().getTime());
 
 populrme.close = function() {
+  var i;
   if (typeof window.postMessage == 'function') {
     window.removeEventListener('message', populrme.receiveMessage, false);
   } else if (typeof window.postMessage == 'object') {
     window.detachEvent('onmessage', populrme.receiveMessage);
   }
-  var i;
   for (i=0; i<populrme.elements.length; i++) {
     document.body.removeChild(populrme.elements[i]);
   }
@@ -24,72 +23,29 @@ populrme.mineImages = function () {
   var images = document.getElementsByTagName('img');
   var i, img, width, height;
   var tempImage;
-  var imageData = [];
+  populrme.images = [];
+
   for (i=0; i<images.length; i++) {
     img = images[i];
     tempImage = new Image();
     tempImage.src = img.src;
     width = tempImage.width;
     height = tempImage.height;
+
     if (img.src.length < 1700 && width >= 100 && height >= 100) {
-      imageData.push({ width: width, height: height, encodedSrc: encodeURIComponent(img.src) });
+      populrme.images.push({ width: width, height: height, source: img.src });
     }
+
   }
-  return imageData;
 }
 
-populrme.paramsFromMessage = function(message, i) {
-  var istr = String(i);
-  return  '&' + 'w' + istr + '=' + String(message.width) + '&h' + istr + '=' + String(message.height) + '&s' + istr + '=' + message.encodedSrc;
-}
-
-populrme.constructMessages = function(imageData) {
-  var i, params;
-  var baseUrl = populrme.protocol + '//' + populrme.appHost + '/collection_candidate?id=' + populrme.id;
-  var messages = [];
-  while (imageData.length > 0) {
-    i = 0;
-    params = '';
-    while (imageData.length > 0 && (baseUrl + params + populrme.paramsFromMessage(imageData[0], i)).length < 2048) {
-      params += populrme.paramsFromMessage(imageData.shift(), i++);
-    }
-    messages.push(baseUrl + params);
-  }
-  return messages;
-}
-
-populrme.sendMessages = function(messages) {
-  if (messages.length == 0) {
-    alert("Couldn't find any images on this page that are large enough to capture");
-    populrme.close();
-    return;
-  }
-  var i, img, div;
-
-  populrme.loaderTimeoutID = setTimeout(populrme.showLoader, 2000);
-
-  populrme.messageCount = messages.length;
-
-  div = document.createElement('div');
-  populrme.elements.push(div);
-  div.style.position = 'absolute';
-  div.style.top = '0';
-  div.style.left = '-10px';
-  div.style.width = '1px';
-
-  for (i=0; i<messages.length; i++) {
-    img = document.createElement('img');
-    img.onload = function() { populrme.messageComplete(); };
-    img.src = messages[i];
-    div.appendChild(img);
-  }
-
-  document.body.appendChild(div);
-}
-
-populrme.messageComplete = function() {
-  if (--populrme.messageCount == 0) {
-    populrme.showIFrame();
+populrme.sendMessage = function(message) {
+  if (typeof(window.postMessage) == 'function') {
+    window.frames['populrme_asset_collector'].postMessage(message, '*');
+  } else if (typeof(window.postMessage) == 'object') {
+    window.setTimeout(function() {
+      window.frames['populrme_asset_collector'].postMessage(message, '*')
+    }, 0);
   }
 }
 
@@ -117,19 +73,23 @@ populrme.getViewportWidth = function() {
   }
 }
 
-populrme.checkIFrameUrl = function () {
-  if (populrme.iframe.src.split('#')[1] == 'close') {
-    populrme.close();
-  } else {
-    populrme.setCloseCheckTimeout();
+populrme.populateCollector = function () {
+  var i, data;
+
+  for (i=0; i<populrme.images.length; i++) {
+    data = populrme.images[i];
+    populrme.sendMessage(data.width + '|' + data.height + '|' + data.source);
   }
+  populrme.sendMessage('done');
 }
 
 populrme.receiveMessage = function(event) {
   if (true || event.origin == 'https://populr.me' ||
       event.origin == 'https://staging.populr.me' ||
       event.origin == 'http://lvh.me:3000') {
-    if (event.data == 'close_populr_asset_collector') {
+    if (event.data == 'populr_asset_collector_ready') {
+      populrme.populateCollector()
+    } else if (event.data == 'close_populr_asset_collector') {
       populrme.close();
     }
   }
@@ -141,79 +101,48 @@ populrme.setupCallback = function() {
   } else if (typeof window.postMessage == 'object') {
     window.attachEvent('onmessage', populrme.receiveMessage);
   } else {
-    populrme.setCloseCheckTimeout();
+    alert('The Push2Pop bookmarklet does not support this browser');
+    populrme.close();
   }
 }
 
-populrme.setCloseCheckTimeout = function() {
-  setTimeout(populrme.checkIFrameUrl, 100);
-}
-
-
-populrme.applySharedAttributes = function(block) {
-  block.style.position = 'fixed';
-  block.style.left = '0';
-  block.style.width = '100%';
-  block.style.zIndex = 99999;
-}
-
-populrme.showLoader = function() {
-  var div, loader, body, html;
-  body = document.body;
-  html = document.documentElement;
-
-  div = document.createElement('div');
-  populrme.elements.push(div);
-  populrme.applySharedAttributes(div);
-  div.style.top = '45%';
-  div.style.textAlign = 'center';
-  div.style.fontSize = '48px';
-  div.style.borderRadius = '10px';
-  div.style.boxShadow = '0 5px 5px rgba(0, 0, 0, 0.75)';
-  div.style.backgroundColor = '#eee';
-  div.style.paddingTop = '50px';
-  div.style.height = '90px';
-  div.style.width = '140px';
-  div.style.left = String(populrme.getViewportWidth() / 2 - 70) + 'px';
-
-  loader = document.createElement('img');
-  loader.src = populrme.protocol + '//' + populrme.appHost + '/images/tinyloader-dark.gif'
-  div.appendChild(loader);
-
-  document.body.appendChild(div);
-}
 populrme.showIFrame = function() {
   var iframe, body, html;
 
-  clearTimeout(populrme.loaderTimeoutID);
+  populrme.originalDocOverflowStyle = document.documentElement.style.overflow;
+  populrme.originalBodyScroll = document.body.scroll;
+  document.documentElement.style.overflow = 'hidden';
+  document.body.scroll = 'no'; // ie
+
 
   body = document.body;
   html = document.documentElement;
 
   iframe = document.createElement('iframe');
   populrme.elements.push(iframe);
-  populrme.applySharedAttributes(iframe);
+  iframe.style.position = 'fixed';
+  iframe.style.left = '0';
+  iframe.style.width = '100%';
+  iframe.style.zIndex = 99999;
   iframe.style.top = '0';
   iframe.style.height = String(populrme.getViewportHeight()) + 'px';
   iframe.style.border = '0';
   iframe.frameborder = 0;
   // iframe.border = 0;
-  iframe.src = populrme.protocol + '//' + populrme.appHost + '/asset_collector?id=' + populrme.id;
+  iframe.src = populrme.protocol + '//' + populrme.appHost + '/asset_collector';
+  iframe.name = 'populrme_asset_collector'
   populrme.iframe = iframe;
   document.body.appendChild(iframe);
-
-  populrme.setupCallback();
 }
 
-populrme.displayCollector = function () {
-  populrme.originalDocOverflowStyle = document.documentElement.style.overflow;
-  populrme.originalBodyScroll = document.body.scroll;
-  document.documentElement.style.overflow = 'hidden';
-  document.body.scroll = 'no'; // ie
-
-  var imageData = populrme.mineImages();
-  var messages = populrme.constructMessages(imageData);
-  populrme.sendMessages(messages);
+populrme.initializeCollector = function() {
+  populrme.mineImages();
+  if (populrme.images.length == 0) {
+    alert("Couldn't find any images on this page that are large enough to capture");
+  } else {
+    populrme.setupCallback();
+    populrme.showIFrame();
+  }
 }
 
-populrme.displayCollector();
+populrme.initializeCollector();
